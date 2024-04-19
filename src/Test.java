e043c057e6761d1b9ecff33b774da71c573bd8f5
PawelJedrzejczaks30640 <162434513+PawelJedrzejczaks30640@users.noreply.github.com>
2024-04-19 21:08:59 +0200

    Update Test.java
    musiałem poprawić mały błąd związany z wielkimi literami

--- src/Test.java
+++ src/Test.java
@@ -5,36 +5,36 @@ class PersonTest {
 
     @Test
     void testZnizkaDziecka() {
-        Osoba osoba = new Osoba(5, "Bydgoscz", "Wtorek");
+        Osoba osoba = new Osoba(5, "bydgoscz", "wtorek");
         assertEquals(100, osoba.oblcizanieZnizki());
     }
 
     @Test
     void testLiczenieZnizkiNastolatek() {
-        Osoba osoba = new Osoba(16, "Częstochowa", "Sobota");
+        Osoba osoba = new Osoba(16, "częstochowa", "sobota");
         assertEquals(50, osoba.oblcizanieZnizki());
     }
 
     @Test
     void testZnizkaNastolatekWarszawa() {
-        Osoba osoba = new Osoba(12, "Warszawa", "Poniedziałek");
+        Osoba osoba = new Osoba(12, "warszawa", "poniedziałek");
         assertEquals(60, osoba.oblcizanieZnizki());
     }
 
     @Test
     void testZnizkaCzwartkowa() {
-        Osoba osoba = new Osoba(29, "Piotrków", "Czwartek");
+        Osoba osoba = new Osoba(29, "piotrków", "czwartek");
         assertEquals(100, osoba.oblcizanieZnizki());
     }
 
     @Test
     void testZnizkaOsobaDorosla() {
-        Osoba osoba = new Osoba(43, "Kraków","Poniedzialek");
+        Osoba osoba = new Osoba(43, "kraków","poniedzialek");
         assertEquals(0, osoba.oblcizanieZnizki());
     }
     @Test
     void testZnizkaOsobaDorosłaWarszawa() {
-        Osoba osoba = new Osoba(22, "Watszawa","Wtorek");
+        Osoba osoba = new Osoba(22, "warszawa","wtorek");
         assertEquals(0, osoba.oblcizanieZnizki());
     }
 
